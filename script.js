c194148ec54704bb5747f37aa28c7671a66161fd
Jade S <thedavidsoto@gmail.com>
2024-11-15 14:17:59 +0800

    Add operate function

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function multiply(num1, num2) {
 function divide(num1, num2){
     return num1 / num2;
 }
+
+function operate(num1, num2, operation) {
+    switch(operation) {
+        case "add":
+            return add(num1, num2);
+        case "subtract":
+            return subtract(num1, num2);
+        case "multiply":
+            return multiply(num1, num2);
+        case "divide":
+            return divide(num1, num2);
+    }
+}
