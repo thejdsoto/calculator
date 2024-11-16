f0f91ba075707c867d2ef5f763c6091631dcf1e1
Jade S <thedavidsoto@gmail.com>
2024-11-16 17:59:29 +0800

    Add code that will round off answers to nearest two decimal places

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function add(num1, num2) {
-    return num1 + num2;
+    return Math.round((num1 + num2) * 100) / 100;
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return Math.round((num1 - num2) * 100) / 100;
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    return Math.round((num1 * num2) * 100) / 100;
 }
 
 function divide(num1, num2){
-    return num1 / num2;
+    return Math.round((num1 / num2) * 100) / 100;
 }
 
 function operate(num1, num2, operation) {
