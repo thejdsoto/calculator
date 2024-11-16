ab2b2751d1a3053d322e80d774f4805b0cbbad1d
Jade S <thedavidsoto@gmail.com>
2024-11-16 18:18:01 +0800

    Correct wrong function name clear() to clearData()

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function multiply(num1, num2) {
 
 function divide(num1, num2){
     if (num2 === 0) { 
-        clear();
+        clearData();
         return "CANNOT DIVIDE BY ZERO!" 
     } else {
         return Math.round((num1 / num2) * 100) / 100;
